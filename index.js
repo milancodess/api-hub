77d537f3aa004b1460f4d742dc89aebf85c4a4e9
Milan Bhandari <108926680+milancodess@users.noreply.github.com>
2025-10-08 02:37:04 +0545

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+// index.js
+import * as anime from "./routes/anime/index.js";
+import * as downloader from "./routes/downloader/index.js";
+import * as tools from "./routes/tools/index.js";
+import * as news from "./routes/news/index.js";
+
+export { anime, downloader, tools, news };
