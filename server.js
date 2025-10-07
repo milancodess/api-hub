643f096ed84d847cf018dfa08c897c14eeb2bf65
Milan Bhandari <108926680+milancodess@users.noreply.github.com>
2025-10-08 02:41:04 +0545

    Initial commit

--- server.js
+++ server.js
@@ -1,18 +1,7 @@
-// server.js
 import express from "express";
-import animeRoutes from "./routes/anime/index.js";
-import downloaderRoutes from "./routes/downloader/index.js";
-import toolsRoutes from "./routes/tools/index.js";
-import newsRoutes from "./routes/news/index.js";
 
 const app = express();
 app.use(express.json());
-
-// Register category routes
-app.use("/api/anime", animeRoutes);
-app.use("/api/downloader", downloaderRoutes);
-app.use("/api/tools", toolsRoutes);
-app.use("/api/news", newsRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
